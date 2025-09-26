c2100f43113558366ea7c955645eef46aced5a22
Olikli55 <89693729+Olikli55@users.noreply.github.com>
2025-09-26 15:46:59 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,49 +1,45 @@
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
-
+#include <vector>
 #include"shaderClass.h"
 #include"VAO.h"
 #include"VBO.h"
 #include"EBO.h"
+#include <cstdlib>
+#include <ctime>
 
-const float blockSize= 0.05f;
+const float blockSize = 0.05f;
 
+short grid[20][20];
 
 // Vertices for the fricking triangle
-GLfloat vertices[] =
-{ //               COORDINATOS                  /     COLORES           //
-	0.0f , 0.0f , 0.0f ,          1.0f, 0.0f, 0.0f,
-	0.0f , 0.05f , 0.0f ,		  1.0f, 0.5f, 0.0f,
-	0.05f , 0.0f , 0.0f ,		  1.0f, 0.5f, 0.2f,
-	0.05f , 0.05f , 0.0f ,		  1.0f, 0.0f, 0.5f,
-
-
-	0.1f , 0.0f , 0.0f ,		  1.0f, 0.5f, 0.2f,
-	0.1f , 0.05f , 0.0f ,		  1.0f, 0.0f, 0.5f,
-	
-
-
-
-};
+std::vector<GLfloat> vertices;
 
 // Indices for vertices order
-GLuint indices[] =
-{
-	0,1,2,
-	1,2,3,
-	2,3,4,
-	3,4,5,
-
-	
-};
+std::vector<GLuint> indices;
 
 
-
+void blockToVerticies(float x, float y);
 
 
 int main()
 {
+	std::srand(static_cast<unsigned int>(std::time(nullptr))); // Seed RNG
+	blockToVerticies(0.0f, 0.0f);
+
+	for (signed short x = -10; x < 10; x++){
+		for (signed short y = -10; y < 10; y++){
+			if ((std::rand() % 2) == 1) {
+				blockToVerticies(x * 0.05, y * 0.05);
+			}
+			
+
+		}
+	}
+
+
+
 	// Initialize GLFW
 	glfwInit();
 
@@ -51,7 +47,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); 	// Tell GLFW we are using the CORE profile == modern functions only
 
-	GLFWwindow* window = glfwCreateWindow(800, 800, "TRIANGEL YAYAYAY", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(1000, 900, "TRIANGEL YAYAYAY", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "ERROR: creation of window faild" << std::endl;
@@ -63,7 +59,7 @@ int main()
 	gladLoadGL();
 
 
-	glViewport(0, 0, 800, 800);	// Specify the viewport of OpenGL in the Window. by the way it is FUKCING reversed
+	glViewport(0, 0, 1000, 900);	// Specify the viewport of OpenGL in the Window. by the way it is FUKCING reversed
 
 
 
@@ -77,9 +73,9 @@ int main()
 	VAO1.Bind();
 
 	// Generates Vertex Buffer Object and links it to vertices
-	VBO VBO1(vertices, sizeof(vertices));
+	VBO VBO1(vertices.data(), vertices.size() * sizeof(GLfloat));
 	// Generates Element Buffer Object and links it to indices
-	EBO EBO1(indices, sizeof(indices));
+	EBO EBO1(indices.data(), indices.size() * sizeof(GLuint));
 
 	// Links VBO attributes such as coordinates and colors to VAO
 	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
@@ -92,29 +88,28 @@ int main()
 	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale"); 	// Gets ID of uniform called "scale" no idea why too
 
 
-
 	// Main loop
 	while (!glfwWindowShouldClose(window))
 	{
-		
-		for (int i = 1; i < sizeof(vertices) / sizeof(float); i += 6) {
-			vertices[i] += 0.00001f; // Only update the Y coordinate
+
+		for (unsigned short i = 1; i < vertices.size(); i += 6) {
+			//vertices[i] -= 0.0001f; // Only update the Y coordinate
 		}
-		
-		
+
+
 
 
 		VBO1.Bind();
-		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, vertices.size() * sizeof(GLfloat), vertices.data());
 		VBO1.Unbind();
-		
+
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);    // Specify the color of the background
 		glClear(GL_COLOR_BUFFER_BIT);              // Clean the back buffer and assign the new color to it
 		shaderProgram.Activate();				  // activate the sader IDK
 		glUniform1f(uniID, 0.5f);			     // Assigns a value to the uniformNOTE: Must always be done after activating the Shader Program
 		VAO1.Bind();                        	// Bind the VAO so OpenGL knows to use it
-		                                       // Draw primitives, number of indices, datatype of indices, index of indices
-		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+		// Draw primitives, number of indices, datatype of indices, index of indices
+		glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);		// Swap the back buffer with the front buffer
 		glfwPollEvents();			   // Take care of the pooled events 
 	}
@@ -132,7 +127,31 @@ int main()
 	shaderProgram.Delete();
 	// Delete window before ending the program
 	glfwDestroyWindow(window);
-	// Terminate GLFW before ending the program
+	// Terminate GLFW tqybgtgtbefore ending the program
 	glfwTerminate();
 	return 0;
+}
+
+
+
+
+void blockToVerticies(float x, float y) {
+	vertices.insert(vertices.end(), { x, y, 0.0f, 1.0f, x, y });
+	vertices.insert(vertices.end(), { x, y + blockSize, 0.0f, 1.0f, 0.2f, 0.2f });
+	vertices.insert(vertices.end(), { x + blockSize, y, 0.0f, 1.0f, y, x });
+	vertices.insert(vertices.end(), { x + blockSize, y + blockSize, 0.0f, x, 0.2f, y });
+
+	GLuint lastINdicie = 0;
+	if (!indices.empty()) {
+		lastINdicie = indices.back() + 1;
+	}
+
+	indices.insert(indices.end(), {
+		lastINdicie + 0,lastINdicie + 1, lastINdicie + 2,
+		lastINdicie + 1,lastINdicie + 2,lastINdicie + 3,
+
+
+		});
+
+
 }
